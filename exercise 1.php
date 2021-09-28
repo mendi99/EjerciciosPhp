05c0c139fb6388081bae4e470ba389fc2ca1dcab
mendi99 <mmendizabalsa20dw@ikzubirimanteo.com>
2021-09-28 12:46:23 +0200

    ejercicio1

--- exercise 1.php
+++ exercise 1.php
@@ -1,11 +1,24 @@
 <html>
- <head>
- <title>Prueba de PHP</title>
- </head>
- <body>
-    <?php
-    $num1 = 4;
-    
-    ?>        
- </body>
+   <head>
+   <title>Prueba de PHP</title>
+   </head>
+   <body>
+      <?php
+         $num1 = 12;
+         $num2 = 5;
+         $arrayAlfabeto = alfabeto();
+
+         for($i = 1; $i <= $num1; $i++){
+            for($j = 0; $j < $num2; $j++){
+               echo "piso $i letra ".$arrayAlfabeto[$j]."\t";
+            }
+            echo "<br>";
+         }
+
+         function alfabeto(){
+            return array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+         }
+         
+      ?>        
+   </body>
 </html>
---
+++ exercise 2.php
@@ -0,0 +1,15 @@
+<html>
+   <head>
+   <title>Prueba de PHP</title>
+   </head>
+   <body>
+      <?php
+         $num1 = 12;
+         $num2 = 5;
+         $num3 = 10;
+
+         echo max($num1, $num2, $num3);
+         
+      ?>        
+   </body>
+</html>
