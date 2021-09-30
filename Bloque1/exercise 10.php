5d233909cbea24d9be02f8f701d8127b6c71e4af
mendi99 <mmendizabalsa20dw@ikzubirimanteo.com>
2021-09-30 12:50:16 +0200

    Cambio de nombre

--- Bloque1/exercise 10.php
+++ Bloque1/exercise 10.php
@@ -7,9 +7,35 @@
    <body>
       <?php
 
-      $ventas = 60;
-      
+      class Tienda{
 
+         private $total_compra;
+         private $tipo_compra;
+
+         function __construct($total_compra, $tipo_compra){
+            $this->total_compra = $total_compra;
+            $this->tipo_compra = $tipo_compra;
+         }
+
+         
+         function analizarCompra(){
+            if($this->total_compra < 19){
+               if($this->tipo_compra == 'ropa'){
+                  echo "Los gastos de envío son 9 euros";
+               }else if($this->tipo_compra == 'mascotas'){
+                  echo "No se puede realizar el envío";
+               }
+            }else if($this->total_compra < 40){
+               echo "Los gastos de envío son 9 euros";
+            }else if($this->total_compra >= 40){
+               echo "Los portes de envío son gratis";
+            }
+         }
+      
+      }
+
+      $t1 = new Tienda(50, 'ropa');
+      $t1->analizarCompra();
       ?>        
    </body>
 </html>
